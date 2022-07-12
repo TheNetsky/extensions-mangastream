bc845068980bb28c81b49be4967fac0e25a00459
Rushi Patel <rnpnull@gmail.com>
2022-07-11 21:20:52 -0400

    Created LuminousScans source, partial progress on selector setup (manga details/genre selectors)

---
+++ src/LuminousScans/LuminousScans.ts
@@ -0,0 +1,61 @@
+/* eslint-disable linebreak-style */
+import {
+    LanguageCode,
+    SourceInfo,
+    ContentRating,
+} from "paperback-extensions-common";
+
+import { MangaStream, getExportVersion } from "../MangaStream";
+
+const LUMINOUSSCANS_DOMAIN = "https://luminousscans.com/";
+
+export const LuminousScansInfo: SourceInfo = {
+    version: getExportVersion("0.0.0"),
+    name: "LuminousScans",
+    description: "Extension that pulls manga from LuminousScans",
+    author: "yehru",
+    authorWebsite: "http://github.com/yehrupx",
+    icon: "logo.png",
+    contentRating: ContentRating.MATURE,
+    websiteBaseURL: LUMINOUSSCANS_DOMAIN,
+};
+
+export class LuminousScans extends MangaStream {
+    //FOR ALL THE SELECTIONS, PLEASE CHECK THE MangaSteam.ts FILE!!!
+
+    baseUrl: string = LUMINOUSSCANS_DOMAIN;
+    languageCode: LanguageCode = LanguageCode.ENGLISH;
+
+    override sourceTraversalPathName = "series";
+
+    // need to change alternative titles selection w parser
+
+    //----MANGA DETAILS SELECTORS
+    /*
+    If a website uses different names/words for the status below, change them to these.
+    These must also be changed id a different language is used!
+    Don't worry, these are case insensitive.
+    */
+
+    //manga_StatusTypes: object = {
+    //    ONGOING: "ongoing",
+    //    COMPLETED: "completed"
+    //}
+
+    //----HOMESCREEN SELECTORS
+    //Disabling some of these will cause some Home-Page tests to fail, edit these test files to match the setting.
+    //Always be sure to test this in the app!
+
+    override homescreen_PopularToday_enabled = true;
+    override homescreen_LatestUpdate_enabled = true;
+    override homescreen_NewManga_enabled = false;
+    override homescreen_TopAllTime_enabled = true;
+    override homescreen_TopMonthly_enabled = true;
+    override homescreen_TopWeekly_enabled = true;
+
+    //----TAG SELECTORS
+    override tags_SubdirectoryPathName = "/series/";
+    override tags_selector_box = "ul.genrez";
+    override tags_selector_item = "li";
+    override tags_selector_label = "label";
+}
